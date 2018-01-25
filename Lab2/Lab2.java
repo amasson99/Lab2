625bdf770c002e2c6b488b46199454579b8c0490
Alex Masson <amasson99@gmail.com>
2018-01-25 16:15:54 -0600

    changes

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -5,25 +5,22 @@ public class Lab2 {
 
     Scanner in = new Scanner (System.in);// Declare a Scanner object here to be used for input
 
-    double creditHours1;
+    double creditHours = 0;
     String letterGrade1;
-    int total;
-
-
-    /*double creditHours2;
-    double creditHours3;
-    double creditHours4;*/
-
-    String input;
+    double total = 0;
+    double dec = 0;
+    String input ;
 
     System.out.print("Please enter credit hours for the course(1): ");
 			input = in.nextLine();
-      creditHours1 = Double.parseDouble(input);
+      creditHours += Double.parseDouble(input);
 
     System.out.print("Please enter the letter grade recieved for the course(1): ");
   		letterGrade1 = in.nextLine();
 
-      if (letterGrade1.equals("A") || letterGrade1.equals("a")){
+
+
+    if (letterGrade1.equals("A") || letterGrade1.equals("a")){
         dec = 4;
         total += dec;
 
@@ -39,23 +36,15 @@ public class Lab2 {
         dec = 1;
         total += dec;
 
-    } else if (letterGrade1.equals("F") || letterGrade1.equals("f")){
+    } else {
         dec = 0;
         total += dec;
-      }//end of if
+    }//end of if
 
 
-      double GPA = total / classes;
+    double GPA = (total * creditHours) / creditHours;
 
-      DecimalFormat formatter = new DecimalFormat("0.##");
-      System.out.println( formatter.format("Your GPA is " + GPA));
-
-    /*System.out.print("Please enter credit hours for the course(2): ");
-  		creditHours2 = in.nextLine();
-    System.out.print("Please enter credit hours for the course(3): ");
-    	creditHours3 = in.nextLine();
-    System.out.print("Please enter credit hours for the course(4): ");
-      creditHours4 = in.nextLine();*/
+    System.out.printf("Your GPA is %.2f\n", GPA);
 
     in.close();//close the scanner
   }
